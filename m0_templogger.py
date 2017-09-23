2c1240f45eaff3f957514b53d29bdf7fb8b1f3ed
Jerry Needell <jerryneedell@gmail.com>
2017-09-23 07:38:13 -0400

    update example

--- m0_templogger.py
+++ m0_templogger.py
@@ -6,15 +6,27 @@ import time
 led = digitalio.DigitalInOut(board.D13)
 led.switch_to_output()
 
+#create logging interval counter
+logInterval = 0
+
 try:
     with open("/temperature.txt", "a") as fp:
         while True:
-            temp = microcontroller.cpu.temperature
-            # do the C-to-F conversion here if you would like
-            fp.write('{0:f}\n'.format(temp))
-            fp.flush()
+            if(logIntererval == 60)
+                temp = microcontroller.cpu.temperature
+                # do the C-to-F conversion here if you would like
+                fp.write('{0:f}\n'.format(temp))
+                fp.flush()
+                # 5 quick blinks
+                while (blinkLoop in range(5):
+                    led.value = not led.value
+                    time.sleep(.2)
+                # reset Interval counter
+                logInterval = 0
+            #blink the led
             led.value = not led.value
             time.sleep(1)
+            logInterval += 1
 except OSError as e:
     delay = 0.5
     if e.args[0] == 28:
