7dd51bd8a67734971d975269f89c9d8c01039ab2
Jerry Needell <jerryneedell@gmail.com>
2017-09-23 08:06:04 -0400

    update example

--- code.py
+++ code.py
@@ -1,24 +1,38 @@
-import digitalio
 import board
+import digitalio
 import microcontroller
 import time
-
-switch=digitalio.DigitalInOut(board.D0)
-switch.switch_to_input(pull=digitalio.Pull.UP)
 
 led = digitalio.DigitalInOut(board.D13)
 led.switch_to_output()
 
-if switch.value==False: # if D0 is pulled low, then datalog mode -- no USB write allowed
-    for i in range(10): # keep number of writes low for testing
-        f=open('temp_log.txt','a')
-        temp=microcontroller.cpu.temperature
-        f.write('{0:03d}  {1:.3f}\n'.format(i,temp))
-        f.close()
-        led.value = not led.value # blink to indicate successful write
-        time.sleep(1)
-        
-else:
+#create logging interval counter
+logInterval = 60
+logCountdown = 0
+
+try:
+    with open("/temperature.txt", "a") as fp:
+        while True:
+            if(logCountdown <= 0):
+                temp = microcontroller.cpu.temperature
+                # do the C-to-F conversion here if you would like
+                fp.write('{0:f}\n'.format(temp))
+                fp.flush()
+                # 5 quick blinks
+                for blinkLoop in range(0,5):
+                    led.value = not led.value
+                    time.sleep(.2)
+                # reset Interval counter
+                logCountdown = logInterval
+            #blink the led
+            led.value = not led.value
+            time.sleep(1)
+            logCountdown -= 1
+except OSError as e:
+    delay = 0.5
+    if e.args[0] == 28:
+        delay = 0.25
     while True:
-        led.value = not led.value #faster blink to indicate in USB read-write mode
-        time.sleep(.2)
+        led.value = not led.value
+        time.sleep(delay)
+
